d37f13ae548d0cd2398318adf4f433cc99b470bb
Natali <natali1grin@gmail.com>
2022-04-28 12:42:12 +0300

    first commit test

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,7 +1,7 @@
 export function Home() {
   return (
     <section className="home container flex column justify-center align-center">
-      <h2>Welcome!</h2>
+      <h2>Welcome! test gitgi</h2>
     </section>
   );
 }
